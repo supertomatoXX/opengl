773e7e958716db3903a83390efbc7ac1e67b13cc
ckx0042 <ckx0042@175game.com>
2018-03-27 17:11:40 +0800

    new

--- ogldev-source/tutorial17/lighting.fs
+++ ogldev-source/tutorial17/lighting.fs
@@ -10,6 +10,8 @@ struct DirectionalLight
     float AmbientIntensity;
 };
 
+//颜色值和环境光强度通过上面的方式获得：一个是在shader重定义的一致变量的名字（gDirectionalLight），
+//然后用点运算符获取结构体中两个区域的变量。同时设置结构体中的这些变量的值也和其他普通变量一样进行直接赋值。
 uniform DirectionalLight gDirectionalLight;
 uniform sampler2D gSampler;
 
