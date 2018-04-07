1fece46eb25e8754c3cc36c641751c795459b38c
ckx0042 <ckx0042@175game.com>
2018-04-07 11:30:36 +0800

    add gs desc

--- ogldev-source/tutorial27/billboard.fs
+++ ogldev-source/tutorial27/billboard.fs
@@ -8,7 +8,9 @@ out vec4 FragColor;
 void main()                                                                         
 {                                                                                   
     FragColor = texture2D(gColorMap, TexCoord);                                     
-                                                                                    
+    
+
+    //内置的关键字’discard’用于在某些情况下将某些像素片元完全丢弃                                                                                
     if (FragColor.r == 0 && FragColor.g == 0 && FragColor.b == 0) {
         discard;                                                                    
     }                                                                               
