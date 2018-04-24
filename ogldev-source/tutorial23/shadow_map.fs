1b1b8535546f22cbaff084e3bf4a288d37aa3c30
ckx0042 <ckx0042@175game.com>
2018-04-24 17:33:42 +0800

    add new desc

--- ogldev-source/tutorial23/shadow_map.fs
+++ ogldev-source/tutorial23/shadow_map.fs
@@ -8,6 +8,6 @@ out vec4 FragColor;
 void main()                                                                         
 {                                                                                   
     float Depth = texture(gShadowMap, TexCoordOut).x;                               
-    Depth = 1.0 - (1.0 - Depth) * 25.0;                                             
+    Depth = 1.0 - (1.0 - Depth) * 25.0;                                       
     FragColor = vec4(Depth);                                                        
 }
